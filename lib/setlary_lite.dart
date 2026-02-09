f361ecbbeb209d94057fc5dfd0761e50b96aeb63
Lee Shu Hoey <shuhoey@setlary.my>
2026-02-09 12:19:45 +0800

    - Change to InAppWebView

--- lib/setlary_lite.dart
+++ lib/setlary_lite.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class SetlaryLite extends StatefulWidget {
   final String url;
@@ -12,6 +12,9 @@ class SetlaryLite extends StatefulWidget {
 
   /// Optional: set width of the container
   final double? width;
+  final bool hideTitle;
+  final Color? titleColour;
+  final double? fontSize;
 
   const SetlaryLite({
     super.key,
@@ -20,6 +23,9 @@ class SetlaryLite extends StatefulWidget {
     this.enableJavaScript = true,
     this.height,
     this.width,
+    this.hideTitle = false,
+    this.titleColour,
+    this.fontSize,
   });
 
   @override
@@ -27,45 +33,87 @@ class SetlaryLite extends StatefulWidget {
 }
 
 class _SetlaryLiteState extends State<SetlaryLite> {
-  late final WebViewController _controller;
+  late final InAppWebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(
-        widget.enableJavaScript
-            ? JavaScriptMode.unrestricted
-            : JavaScriptMode.disabled,
-      )
-      ..loadRequest(
-        Uri.parse(
-          '${widget.url}?token=${Uri.encodeComponent(widget.token)}',
-        ),
-      );
   }
 
   void _navigateToWebView() {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) =>
-            Scaffold(body: WebViewWidget(controller: _controller)),
+        builder: (context) => Scaffold(
+          body: InAppWebView(
+            key: UniqueKey(),
+            initialUrlRequest: URLRequest(
+              url: WebUri(
+                '${widget.url}?token=${Uri.encodeComponent(widget.token)}',
+              ),
+            ),
+            onWebViewCreated: (controller) {
+              _controller = controller;
+            },
+            initialSettings: InAppWebViewSettings(
+              javaScriptEnabled: true,
+              supportZoom: false,
+              transparentBackground: false,
+              useHybridComposition: true,
+              allowsInlineMediaPlayback: true,
+              allowFileAccess: false,
+            ),
+            onLoadResource: (controller, resource) async {
+              final resourceUrl = resource.url?.toString();
+              if (resourceUrl != null && resourceUrl.endsWith('/close')) {
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _navigateToWebView,
-      child: Container(
-        height: widget.height ?? 48, // default height
-        width: widget.width ?? 48,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: SvgPicture.asset('assets/logo.svg', package: 'setlary_lite',),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            height: widget.height ?? 48, // default height
+            width: widget.width ?? 48,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: SvgPicture.asset(
+              'assets/logo.svg',
+              package: 'setlary_lite',
+              height: 20,
+              width: 20,
+            ),
+          ),
+          if (!widget.hideTitle)
+            Padding(
+              padding: EdgeInsetsGeometry.only(top: 4),
+              child: Text(
+                'Setlary Lite',
+                style: TextStyle(
+                  color: widget.titleColour ?? Colors.black87,
+                  fontSize: widget.fontSize ?? 12,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
